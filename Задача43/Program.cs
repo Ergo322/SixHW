62f7ee4a2c840139ad65b6d7cc68dd0c125f2a2b
Ergo322 <Shikmen2@mail.ru>
2022-09-11 23:54:41 +0300

    task43

---
+++ Задача43/Program.cs
@@ -0,0 +1,17 @@
+﻿void point()
+{
+    Console.WriteLine("Введите начальную координату первой прямой ");
+    decimal b1 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите начальную координату второй прямой ");
+    decimal k1 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите завершающую координату первой прямой ");
+    decimal b2 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите завершающую координату второй прямой ");
+    decimal k2 = Convert.ToInt32(Console.ReadLine());
+
+    decimal x = (b1 - b2) / (k2 - k1);
+    decimal y = (k2 * b1 - k1 * b2) / (k2 - k1);
+
+    Console.WriteLine($"b1 = {b1}, k1 = {k1}, b2 = {b2}, k2 = {k2} -> ({x}; {y})");
+}
+point();
