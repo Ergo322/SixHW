0d8d0c7676bfb2ac54ecb210e27d47e2efef76cb
Ergo322 <Shikmen2@mail.ru>
2022-09-11 23:53:34 +0300

    task41

---
+++ Задача41/Program.cs
@@ -0,0 +1,17 @@
+﻿void PositivNumbers()
+{
+    Console.WriteLine("Введите числа которые хотите видеть в списке(через пробел) ");
+    string arrayNumber = Console.ReadLine();
+    int[] array = arrayNumber.Split(' ').Select(int.Parse).ToArray();
+
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count++;
+        }
+    }
+    Console.WriteLine(count);
+}
+PositivNumbers();
